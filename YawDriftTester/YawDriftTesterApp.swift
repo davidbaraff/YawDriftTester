50f9bc34a3238a6483d1fe2a9c0e1019d529d26c
David Baraff <nosuch@nowhere>
2025-06-09 14:40:42 -0700

    Initial Commit

---
+++ YawDriftTester/YawDriftTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  YawDriftTesterApp.swift
+//  YawDriftTester
+//
+//  Created by David Baraff on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct YawDriftTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
